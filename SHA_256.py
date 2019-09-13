d2d0b1150ef6c2062f0a081d9a33806922d6960d
Vasudevan <vasu3797@gmail.com>
2019-09-13 12:23:48 -0500

    trial of sha512

--- SHA_256.py
+++ SHA_256.py
@@ -1,15 +1,17 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-import sys
+
+
 def SHA_256(message):
-    digest = hashes.Hash(hashes.SHA3_256(), backend=default_backend())
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
     digest.update(message)
-    ciphertext = digest.finalize()
-    return ciphertext
+    msg_digest = digest.finalize()
+    return msg_digest
 
 
 if __name__ == "__main__":
-    cipher = SHA_256(b"Hello World!")
-    print(cipher.hex())
-    x = type(cipher)
-    print(x)
+    plaintext = input("enter plaintext here:")
+    byte_plaintext = plaintext.encode()
+    digest = SHA_256(byte_plaintext)
+    digest = digest.hex()
+    print("The message digest after SHA256 hashing in hexadecimal is: {} ".format(digest))
