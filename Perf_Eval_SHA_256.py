1ca8978962dc36363ade8c9e15ec62c9f8212d5f
Vasudevan <vasu3797@gmail.com>
2019-09-14 09:24:57 -0500

    evaluation of SHA256

---
+++ Perf_Eval_SHA_256.py
@@ -0,0 +1,19 @@
+
+from sys import getsizeof
+import timeit
+
+setup_code = '''
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+
+'''
+
+evaluation_code = '''
+digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+digest.update(bytes(256*1000*1000))
+digest.finalize()
+'''
+
+if __name__ == "__main__":
+    print(timeit.timeit(setup=setup_code, stmt=evaluation_code, number=1))
