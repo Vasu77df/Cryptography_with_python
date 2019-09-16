19323ef97f43034d5aeb6a6c5f0ebefa8f8f664a
vasu3797@gmail.com <vasu3797@gmail.com>
2019-09-16 14:17:43 -0500

    Commenting and finalizing the Hashing code

--- SHA_512.py
+++ SHA_512.py
@@ -1,17 +1,18 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend # importing backend for the hashing algorithms
+from cryptography.hazmat.primitives import hashes # importing hashing functions
 
 
 def SHA_512(message):
-    digest = hashes.Hash(hashes.SHA512(), backend=default_backend())
-    digest.update(message)
-    msg_digest = digest.finalize()
+    digest = hashes.Hash(hashes.SHA512(), backend=default_backend()) # Initializing the hashing function to be used 
+    digest.update(message) # Hashing of the message is done
+    msg_digest = digest.finalize() # Finalizing the current context and returning the digest in bytes
     return msg_digest
 
-
 if __name__ == "__main__":
-    plaintext = input("enter plaintext here:")
-    byte_plaintext = plaintext.encode()
-    digest = SHA_512(byte_plaintext)
-    digest = digest.hex()
-    print("The message digest after SHA512 hashing in hexadecimal is: {} ".format(digest))
+    plaintext = input("Enter Plaintext here:") # Enter the plaintext to be hashed 
+    byte_plaintext = plaintext.encode() # Converting the string datatype to bytes datatype for hashing 
+    digest = SHA_512(byte_plaintext) 
+    digest_hex = digest.hex() # Converting the digest returned in bytes format to hexadecimal format 
+    print ("Plaintext entered: {}".format(plaintext))
+    print("Message digest after Hashing in bytes: {}".format(digest))
+    print("Message digest after SHA512 hashing in hexadecimal is: {} ".format(digest_hex))
