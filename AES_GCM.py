437d27744d5dc0c1f0ee07dee7516a356a502b94
vasu3797@gmail.com <vasu3797@gmail.com>
2019-09-18 12:20:46 -0500

    AESGCM initial dev

--- AES_GCM.py
+++ AES_GCM.py
@@ -1,1 +1,18 @@
-import
+import os 
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+def AES_GCM(data, aad):
+    key = AESGCM.generate_key(bit_length=128)
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    ct = aesgcm.encrypt(nonce, data, aad)
+    pt =aesgcm.decrypt(nonce, ct, aad)
+    return ct, pt
+
+
+if __name__ == "__main__":
+    data = b"Hello World!"
+    aad = b"authenticated but unencrypted data"
+    encrypted_msg, decryted_msg = AES_GCM(data, aad)
+    print(encrypted_msg.hex())
+    print(decryted_msg)
